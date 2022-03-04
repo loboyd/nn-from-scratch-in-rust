46d7a9438fbaaf12b50f5927216321e029d9f771
loboyd <logan.boyd@protonmail.com>
2022-03-03 20:39:22 -0500

    Add MNIST data reader

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 mod matrix;
+mod mnist_reader;
 mod perceptron;
 mod utils;
 
 use crate::matrix::Matrix;
+use crate::mnist_reader::get_data;
 use crate::perceptron::Perceptron;
 use crate::utils::RandomNumberGenerator;
 
 fn main() {
-    let mut rng = RandomNumberGenerator::with_seed(98.);
+    let data = get_data();
+    for x in data.iter().take(6) {
+        println!("\n{}", x);
+    }
 
-    let layer1 = Matrix::random(3, 16, &mut rng);
-    let layer2 = Matrix::random(16, 1, &mut rng);
+    //let mut rng = RandomNumberGenerator::with_seed(98.);
 
-    let perceptron = Perceptron {
-        n_inputs: 3,
-        n_outputs: 1,
-        layers: vec![layer1.clone(), layer2.clone()],
-    };
+    //let layer1 = Matrix::random(3, 16, &mut rng);
+    //let layer2 = Matrix::random(16, 1, &mut rng);
 
-    println!("layer 1:\n{}", layer1);
-    println!();
-    println!("layer 2:\n{}", layer2);
+    //let perceptron = Perceptron {
+    //    n_inputs: 3,
+    //    n_outputs: 1,
+    //    layers: vec![layer1.clone(), layer2.clone()],
+    //};
 
-    let output = perceptron.feed_forward(vec![2., 3., 4.]);
+    //println!("layer 1:\n{}", layer1);
+    //println!();
+    //println!("layer 2:\n{}", layer2);
 
-    println!("{:?}", output);
+    //let output = perceptron.feed_forward(vec![2., 3., 4.]);
+
+    //println!("{:?}", output);
 }
---
+++ src/mnist_reader.rs
@@ -0,0 +1,47 @@
+/// Borrowed from https://www.reddit.com/r/rust/comments/dekpl5/how_to_read_binary_data_from_a_file_into_a_vecu8/
+
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+
+pub struct Image {
+    data: Vec<u8>,
+}
+
+impl std::fmt::Display for Image {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.data
+                .chunks(28)
+                .map(|row| row
+                    .iter()
+                    .map(|pixel| format!("{:4.2}", pixel))
+                    .collect::<Vec<String>>()
+                    .join(" "))
+                .collect::<Vec<String>>()
+                .join("\n")
+        )
+    }
+}
+
+fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
+    let mut f = File::open(&filename).expect("no file found");
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    buffer
+}
+
+pub fn get_data() -> Vec<Image> {
+    let data = get_file_as_byte_vec(&"../mnist/train-images-idx3-ubyte".to_string());
+    data
+        .into_iter()
+        .skip(13)
+        .collect::<Vec<u8>>()
+        .chunks(28*28)
+        .map(|x| Image { data: x.to_vec(), })
+        .collect::<Vec<Image>>()
+}
