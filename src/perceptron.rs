3a9cb785b3a6e6b1c3b7b91bc8cc5f1216c471cf
loboyd <logan.boyd@protonmail.com>
2022-03-04 11:48:13 -0500

    Implement cost function

--- src/perceptron.rs
+++ src/perceptron.rs
@@ -20,4 +20,13 @@ impl Perceptron {
 
         x.data[0].clone()
     }
+
+    /// Compute the cost according a given training pair
+    fn cost(&self, input: Vec<f32>, label: Vec<f32>) -> f32 {
+        let output = self.feed_forward(input);
+
+        let diff_vector = label.iter().zip(output).map(|(x, y)| x - y).collect::<Vec<f32>>();
+
+        diff_vector.iter().map(|x| x * x).sum::<f32>()
+    }
 }
